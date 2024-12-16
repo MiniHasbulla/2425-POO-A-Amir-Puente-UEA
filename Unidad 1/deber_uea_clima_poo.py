86903f303514f70b31f7d2ff76ec0b7f74cda2a4
MiniHasbulla <amirpuente08@gmail.com>
2024-12-15 19:20:40 -0500

    deber_uea_clima_poo.py

--- Unidad 1/deber_uea_clima_poo.py
+++ Unidad 1/deber_uea_clima_poo.py
@@ -1,16 +1,20 @@
+# "Universidad Estatal Amazonica"
+# Clase: POO Paralelo A
+# Nombre: Amir Puente
+#Declaro las clases requeridas
 class ClimaDiario:
     def __init__(self):
         self.temperaturas = []
-
+#La clase registra las temperaturas
     def ingresar_temperaturas(self):
         for i in range(7):
-            temp = float(input(f"Ingrese la temperatura del día {i + 1}: "))
+            temp = float(input(f"Digita la temperatura del día {i + 1}: "))
             self.temperaturas.append(temp)
-
+#Clase para obtener el promedio
     def calcular_promedio(self):
         return sum(self.temperaturas) / len(self.temperaturas)
 
-# Programa principal
+#El programa de manera general
 def main():
     clima = ClimaDiario()
     clima.ingresar_temperaturas()
